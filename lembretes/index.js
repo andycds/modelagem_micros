3db94502263a8416465f9768b3860a919f40a54a
Anderson Sanches <andycds@gmail.com>
2024-11-19 19:21:34 -0300

    first commit

---
+++ lembretes/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+lembretes = {}
+contador = 0
+
+app.get('/oi', (req, res) => {
+    res.send("funcionou!")
+})
+
+app.post('/lembretes', (req, res) => {
+    const { texto } = req.body
+    contador++
+    lembretes[contador] = {
+        contador, texto
+    }
+    res.status(201).send(lembretes[contador])
+})
+
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes)
+})
+
+app.listen(5000, () => console.log("Lembretes. Porta 5000."))
