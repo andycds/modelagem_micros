5da5c37bef875e4ceb3ee9d8f8c7f7a8c507caf1
Anderson Sanches <andycds@gmail.com>
2024-11-19 21:15:30 -0300

    Barramento

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 app.use(express.json())
 
@@ -9,17 +10,29 @@ app.get('/oi', (req, res) => {
     res.send("funcionou!")
 })
 
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
     const { texto } = req.body
     contador++
     lembretes[contador] = {
         contador, texto
     }
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto
+        }
+    })
     res.status(201).send(lembretes[contador])
 })
 
 app.get('/lembretes', (req, res) => {
     res.send(lembretes)
+})
+
+app.post('/eventos', (req, res) => {
+    console.log(req.body)
+    res.send({ msg: 'ok' })
 })
 
 app.listen(4000, () => console.log("Lembretes. Porta 4000."))
--- observacoes/index.js
+++ observacoes/index.js
@@ -1,22 +1,34 @@
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
+const axios = require('axios')
 const app = express()
 app.use(express.json())
 
 const observacoesPorLembreteId = {}
 
-app.post('/lembretes/:id/observacoes', (req, res) => {
+app.post('/lembretes/:id/observacoes', async (req, res) => {
     const idObs = uuidv4()
     const { texto } = req.body;
     const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || []
     observacoesDoLembrete.push({ id: idObs, texto })
     observacoesPorLembreteId[req.params.id] = observacoesDoLembrete
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idObs, texto, lembreteId: req.params.id
+        }
+    })
     res.status(201).send(observacoesDoLembrete)
 })
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
     const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || []
     res.send(observacoesDoLembrete)
+})
+
+app.post('/eventos', (req, res) => {
+    console.log(req.body)
+    res.send({ msg: 'ok' })
 })
 
 app.listen(5000, () => console.log("Observacoes. Porta 5000."))
